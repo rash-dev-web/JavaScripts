c358524d28ed6a68c29ff472918e4e91ee045a7c
Mohammad Rasheed <rasheed.au@gmail.com>
2020-04-03 14:02:02 +0530

    if else and switch

---
+++ Basics/Lecture_3/IfElseTest.js
@@ -0,0 +1,9 @@
+let marks = 25;
+
+if(marks>=65){
+    console.log('First!!');
+}else if(marks >=45 && marks<= 65){
+    console.log('Second!!')
+}else{
+    console.log('Fail!!!');
+}
---
+++ Basics/Lecture_3/SwitchTest.js
@@ -0,0 +1,16 @@
+let role = 'Guest1';
+
+switch(role){
+    case 'Admin': 
+    console.log('Admin User!');
+    break;
+
+    case 'Guest':
+        console.log('Guest User!');
+        break;
+
+    default:
+        console.log('Not Valid User!!');
+
+    
+}
